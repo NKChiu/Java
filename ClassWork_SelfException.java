023f3cdfcc7ad877f1e291a0a7095f238f2c605a
NKChiu <49855959+NKChiu@users.noreply.github.com>
2019-05-17 13:06:31 +0800

    Update ClassWork_SelfException.java

--- ClassWork_SelfException.java
+++ ClassWork_SelfException.java
@@ -5,24 +5,20 @@ package ClassWork;
 ////////CubeException////////////////////////////////////////
 class CubeException extends Exception {
 
+	public CubeException() {}
 
-
-public CubeException() {}
-
-public CubeException(String msg) {
-super(msg);
-}
+	public CubeException(String msg) {
+		super(msg);
+		}
 }
 
 ////////class Cube ////////////////////////////////////////
 class Cube {
-private double length;
+	private double length;
 
-public Cube() {
-	
-}
+	public Cube() {}
 
-public Cube(double length) throws CubeException {  // throws CubeException cannot delete
+	public Cube(double length) throws CubeException {  // throws CubeException cannot delete
 //***************************************************************/			
 //	if(length > 0)								       	//
 //		this.length = length;						  	//
@@ -33,36 +29,33 @@ public Cube(double length) throws CubeException {  // throws CubeException canno
 //////so that's can change following statement//
 	setLength(length);				  //
 /******************************************/		
-}
+	}
 
-public double getLength() {
-	return length;
-}
+	public double getLength() {
+		return length;
+		}
 
-public void setLength(double length) throws CubeException {
+	public void setLength(double length) throws CubeException {
 	if (length > 0)
 		this.length = length;
 	else
 		throw new CubeException("The length is smaller than 0");
-}
+	}
 
-public double getVolume() {
-	return Math.pow(length, 3);
-}
+	public double getVolume() {
+		return Math.pow(length, 3);
+		}
 }
 
 ////////Main ////////////////////////////////////////
 public class ClassWork_SelfException {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-try {
-Cube c1 = new Cube(-10);
-System.out.println(c1.getVolume());
-} catch (CubeException e) {
-e.printStackTrace();
-}
-
-
-}
-
+		try {
+			Cube c1 = new Cube(-10);
+			System.out.println(c1.getVolume());
+		} catch (CubeException e) {
+			e.printStackTrace();
+		}
+	}
 }
